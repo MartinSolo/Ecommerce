61ed7db2244e487d7841ddcbfc348af91033e140
MartinSolo <waitun343@gmail.com>
2023-04-23 01:57:52 +0630

    base

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+
+    public function redirect(){
+        $usertype = Auth::user()->usertype;
+         if ($usertype == "1") {
+            return view("admin.index");
+         }else{
+            return view("dashboard");
+         }
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view("home.userpage");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
